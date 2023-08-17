43cee278bfa4d8f2512c745c2879d87a7f54379e
anudeepreddy <anudeepreddy119@gmail.com>
2023-08-17 17:10:48 +0530

    abc

--- 07-package.sh
+++ 07-package.sh
@@ -4,16 +4,16 @@ PACKAGE=$@
 VALIDATE () {
     if [ $PACKAGE -ne 0 ]
     then
-       echo "$PACKAGE is installed"
+       echo "$1 is installed"
     else
-       echo "$PACKAGE not installed"
+       echo "$1 not installed"
     fi   
 }
 
 yum install nginx -y
 
-VALIDATE $? "already installed"
+VALIDATE $? "already installed nginx"
 
 yum install mysql -y
 
-VALIDATE $? "already installed"
+VALIDATE $? "already installed mysql"
