4cb280739c6c613b0b28a600d24e2c2e8e07bb77
anudeepreddy <anudeepreddy119@gmail.com>
2023-08-21 14:57:05 +0530

    abc

--- diskusage.sh
+++ diskusage.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-# colors
-# validations
-# log redirections
-
-
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-Y="\e[33m"
 
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
-message=""
 
-#IFS= means internal feild seperator is space.
 while IFS= read line
 do  
     # this command will give you usage in number format for comparision
