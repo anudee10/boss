988bd6ad41473ce103341c5a9ef30cedbeda4e78
anudeepreddy <anudeepreddy119@gmail.com>
2023-08-22 10:41:57 +0530

    abc

--- 05-mysql.sh
+++ 05-mysql.sh
@@ -4,15 +4,18 @@ DATE=$(date +%f-%H-%M-%S)
 script_name=$0
 LOGFILE=/tmp/$script_name-$DATE.log
 
+R=\e[31m
+G=\e[32m
+
 USERID=$(id -u)
 
 VALIDATE () {
 if [ $1 -ne 0 ]
  then 
-   echo "$2 is failure"
+   echo "$2 is failure/$G"
    exit 1
  else
-   echo "$2 is success"
+   echo "$2 is success/$R"
  fi
 }
 if [ $USERID -ne 0 ]
